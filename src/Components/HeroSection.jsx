69aa6e38c28306740591be430f767cbd6814dbdb
Shuvayu Patra <shuvayupatra.it2021@nsec.ac.in>
2024-03-05 18:25:19 +0530

    Add new assets and components, delete unused files, and update styles

---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  Button,
+  Container,
+  Heading,
+  Image,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import React from "react";
+import HeroImage from "../Assets/HeroImage.png";
+import { FaShoppingBag } from "react-icons/fa";
+import Vector1 from "../Assets/Vector1.png";
+import Vector2 from "../Assets/Vector2.png";
+
+function HeroSection() {
+  return (
+    <>
+      <Box p={[4, 8]} bgGradient='linear(to-b, #ffffff, #f4f4f5)' pos={"relative"}>
+        <Image src={Vector1} pos={"absolute"} left={0} bottom={'6rem'} />
+        <Image src={Vector2} pos={"absolute"} right={0} bottom={'2rem'}/>
+        <Stack
+          direction={["column", "row"]}
+          maxW={"fit-content"}
+          py={10}
+          justifyContent={"center"}
+          mx={"auto"}
+        >
+          <VStack align={"left"} justifyContent={"center"} order={[2, 1]}>
+            <Heading
+              fontSize="5xl"
+              fontFamily={"'Roboto','sans-serif'"}
+              fontWeight={300}
+              my={4}
+              alignSelf={["center", "flex-start"]}
+            >
+              Collections
+            </Heading>
+            <Container px={1}>
+              <Text
+                fontSize={["md", "xl"]}
+                textTransform={"capitalize"}
+                fontFamily={"'Roboto','sans-serif'"}
+                fontWeight={400}
+                pr={[0, 0]}
+                textAlign={["center", "left"]}
+                // alignSelf={["center", "flex-start"]}
+                lineHeight={"180%"}
+              >
+                You can explore Ans shop many different collection from various
+                brands here
+              </Text>
+            </Container>
+            <Button
+              leftIcon={<FaShoppingBag color="white" />}
+              bgColor={"#1E2832"}
+              variant="solid"
+              color={"white"}
+              size={"lg"}
+              maxW={"fit-content"}
+              _hover={{
+                bgColor: "rgb(30, 40, 50, 0.9)",
+              }}
+              borderRadius={0}
+              my={4}
+              alignSelf={["center", "flex-start"]}
+            >
+              Shop Now
+            </Button>
+          </VStack>
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+            order={[1, 2]}
+          >
+            <Image
+              src={HeroImage}
+              alt="HeroImage"
+              width={["80%", "70%"]}
+              objectFit="cover"
+            />
+          </Box>
+        </Stack>
+      </Box>
+    </>
+  );
+}
+
+export default HeroSection;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,104 @@
+import React from "react";
+import {
+  Button,
+  Drawer,
+  DrawerBody,
+  DrawerContent,
+  DrawerHeader,
+  DrawerOverlay,
+  HStack,
+  Image,
+  Text,
+  VStack,
+  useDisclosure,
+} from "@chakra-ui/react";
+import coral from "../Assets/coral.png";
+import { FaBagShopping, FaUser } from "react-icons/fa6";
+import { CgMenuMotion } from "react-icons/cg";
+const category = [
+  "electronics",
+  "jewelery",
+  "men's clothing",
+  "women's clothing",
+];
+function Navbar() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <HStack
+        bgColor={"#ffffff"}
+        p={[2, 6]}
+        justifyContent={["space-between", "space-evenly"]}
+        alignItems={"center"}
+        borderBottom={'1px solid #e3e3e3'}
+      >
+        <Image src={coral} alt="logo" width={["10rem", "13rem"]} />
+        <HStack display={["none", "flex"]} gap={8} justifyContent={"center"}>
+          {category.map((item, index) => (
+            <Button
+              variant={"unstyled"}
+              textTransform={"capitalize"}
+              key={index}
+            >
+              <Text fontWeight={100} fontFamily={"'Open Sans','sans-serif'"}>
+                {item}
+              </Text>
+            </Button>
+          ))}
+        </HStack>
+        <HStack
+          gap={[2, 8]}
+          justifySelf={"flex-end"}
+          fontFamily={"'Roboto','sans-serif'"}
+        >
+          <Button leftIcon={<FaUser size={20} />} variant="unstyled">
+            <Text display={["none", "inline-block"]}>Account</Text>
+          </Button>
+          <Button leftIcon={<FaBagShopping size={20} />} variant="unstyled">
+            <Text display={["none", "inline-block"]}>Shopping</Text>
+          </Button>
+          <Button
+            display={["inline-block", "none"]}
+            leftIcon={<CgMenuMotion size={20} />}
+            variant="unstyled"
+            onClick={onOpen}
+          >
+            <Text display={["none", "inline-block"]}>Menu</Text>
+          </Button>
+        </HStack>
+      </HStack>
+
+      <Drawer placement={"bottom"} onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay />
+        <DrawerContent borderRadius={'1rem 1rem 0 0'}>
+          <DrawerHeader
+            fontFamily={"'Roboto','sans-serif'"}
+            borderBottomWidth="1px"
+          >
+            Categories
+          </DrawerHeader>
+          <DrawerBody>
+            <VStack gap={4} justifyContent={"center"} alignItems={"start"}>
+              {category.map((item, index) => (
+                <Button
+                  variant={"unstyled"}
+                  textTransform={"capitalize"}
+                  key={index}
+                >
+                  <Text
+                    fontWeight={100}
+                    fontFamily={"'Open Sans','sans-serif'"}
+                  >
+                    {item}
+                  </Text>
+                </Button>
+              ))}
+            </VStack>
+          </DrawerBody>
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+}
+
+export default Navbar;
